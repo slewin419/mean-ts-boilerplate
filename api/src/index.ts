f00be2940756dab95da1b2266eb0f7cc5393ad1e
Kenneth Lewin <kenneth_lewin@wgresorts.com>
2019-12-30 15:00:09 -0500

    connect mongoose to mongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 const port = 3000; // default port to listen
+
+mongoose.connect("mongodb://mongo:27017", {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.once('open', function(){
+  console.log('mongoose connected to mongo!');
+});
+db.on('error', () => {
+  console.log('connection error!');
+});
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
